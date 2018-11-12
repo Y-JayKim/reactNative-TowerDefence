f80d39b70ab9760eac56c762e1929101917fd564
Jay Kim <yeonjae.jay.kim@gmail.com>
2018-11-11 23:11:58 -0800

    menu screen changed

--- components/MenuScreen.js
+++ components/MenuScreen.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TouchableHighlight } from 'react-native';
 
 export default class MenuScreen extends Component {
 
     render() {
         return (
-            <View>
-            <Text>Test</Text>
+            <View style={styles.container}>
+                <Text style={styles.text}>Menu</Text>
+                <TouchableHighlight
+                     style={[styles.button, {top:100}]}
+                     onPress={()=>console.log('button clicked')}>
+                     <Text>Find an Airplane</Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                     style={[styles.button, {top:150}]}
+                     onPress={()=>console.log('button clicked')}>
+                     <Text>Collection</Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                     style={[styles.button, {top:200}]}
+                     onPress={()=>console.log('button clicked')}>
+                     <Text>IDONKNOW</Text>
+                </TouchableHighlight>
             </View>
         )
     }
@@ -18,7 +33,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center'
+  },
+  text:{
+    fontSize:30,
+    top:30
+  },
+  button:{
+    width:200,
+    height:70,
+    borderWidth:0.5, 
+    borderColor:'black',
+    borderRadius:10,
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
