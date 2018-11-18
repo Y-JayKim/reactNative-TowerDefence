955fec3ef70105ffee969c2b6726a19369ca7a4b
koiketani <iketani.ko@gmail.com>
2018-11-18 14:14:47 -0800

    display current region

--- components/MapScreen.js
+++ components/MapScreen.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Text, View, StyleSheet } from 'react-native';
+
 import MapView from 'react-native-maps';
+import Permissions from 'react-native-permissions'
 
 export default class MapScreen extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            locationPermission: 'unknown',
             region: {
                 latitude: 50.60254331180157,
                 latitudeDelta: 0.2729186541296684,
                 longitude: 16.721875704824924,
-                longitudeDelta: 0.26148553937673924
-            }
+                longitudeDelta: 0.26148553937673924,
+            },
         };
         this.onRegionChange = this.onRegionChange.bind(this);
+    }
+
+    componentDidMount() {
+        this._requestPermission();
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(position.coords);
+            let newRegion = {
+                latitude: 0,
+                latitudeDelta: 0.2729186541296684,
+                longitude: 0,
+                longitudeDelta: 0.26148553937673924,
+            };
+            newRegion.latitude = position.coords.latitude;
+            newRegion.longitude = position.coords.longitude;
+            this.setState({
+                region: newRegion,
+            });
+        },
+            (error) => console.log(error));
+    }
+
+    _requestPermission() {
+        Permissions.request('location')
+            .then(response => {
+                this.setState({
+                    locationPermission: response
+                })
+            });
     }
 
     onRegionChange(region){
@@ -28,9 +59,9 @@ export default class MapScreen extends Component {
     render() {
         return (
             <MapView
-            region={this.state.region}
-            onRegionChange={this.onRegionChange}
-            style={styles.map}
+                region={this.state.region}
+                onRegionChange={this.onRegionChange}
+                style={styles.map}
             />
         );
     }
